bdbff569cf5c62bd27293f024929a3fd6ce3bedc
M. Fikri Aulian <03041282126032@student.unsri.ac.id>
2022-08-26 11:19:07 +0700

    add feature check ram usage

--- resources/views/Absen/index.php
+++ resources/views/Absen/index.php
@@ -72,6 +72,19 @@
                 </ul>
             </div>
         </div>
+        <div class="aktivitas-bot">
+            <h4 style="padding-bottom: .5rem;">OS Usage</h4>
+            <div class="aktivitas">
+                <ul>
+                    <?php foreach ($os_usage as $usage) { ?>
+                        <li>
+                            <h6><?= $usage->ram; ?></h6>
+                            <p><?= $usage->cpu; ?></p>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
